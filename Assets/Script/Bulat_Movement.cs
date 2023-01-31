d5a603b32a140e0ef5dde82d600468d14b0cd062
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-31 20:24:46 +0530

    18

--- Assets/Script/Bulat_Movement.cs
+++ Assets/Script/Bulat_Movement.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Bulat_Movement : MonoBehaviour
 {
     [SerializeField]float speed = 5f;
-    public Color color1 = Color.green;
-    
-    private SpriteRenderer spriteRenderer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        
        
     }
 
@@ -21,9 +19,6 @@ public class Bulat_Movement : MonoBehaviour
     {
         transform.position += transform.right * speed * Time.deltaTime;
     }
-  
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        spriteRenderer.color = color1;
-    }
+   
+    
 }
