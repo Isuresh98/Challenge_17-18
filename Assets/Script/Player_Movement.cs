c4561cc6a4929155b3d6b044272aaf136deddffc
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-01-31 18:53:00 +0530

    17 update movement

---
+++ Assets/Script/Player_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private Rigidbody2D _rBody;
+    [SerializeField] float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            _rBody.velocity=(Vector2.up * _speed * Time.deltaTime);
+        }
+
+    }
+}
